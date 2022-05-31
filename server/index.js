5172fe2fd9d2ce799c282ee1de8776ff6c61a919
fred-fittywebdev <contact@fittywebdev.fr>
2022-05-31 15:42:47 +0200

    modifications et insatllations de nouvelles dépendences

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.CONNECTION_URL,
   { useNewUrlParser: true, useUnifiedTopology: true })
@@ -27,4 +27,3 @@ mongoose.connect(process.env.CONNECTION_URL,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-mongoose.set('useFindAndModify', false);
