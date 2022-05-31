1f79e1fff360aeb05bd04799ef5da71fa12d6e87
fred-fittywebdev <contact@fittywebdev.fr>
2022-05-31 15:44:04 +0200

    modifications de certains styles et de l'affichage de la page d'acceuil

---
+++ client/src/components/Home/styles.js
@@ -0,0 +1,11 @@
+import { createStyles, makeStyles } from "@material-ui/core";
+
+export default makeStyles((theme) => ({
+
+    [theme.breakpoints.down('sm')]: {
+        mainContainer: {
+            flexDirection: "column-reverse",
+        }
+    }
+
+}))
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -48,10 +48,10 @@ const Navbar = () => {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
             <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Déconnexion</Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button component={Link} to="/auth" variant="contained" color="primary">Inscription</Button>
         )}
       </Toolbar>
     </AppBar>
