87b941e808ad0423f1dba90a06100c6780816709
fred-fittywebdev <contact@fittywebdev.fr>
2022-06-01 20:45:40 +0200

    Correction du soucis des likes lors de l'update du post, mise en place du rôle admin avec la possibilité pour celui ci d'updater et de supprimer tous les posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,9 +17,9 @@ const Post = ({ post, setCurrentId }) => {
 
   const Likes = () => {
     if (post.likes.length > 0) {
-      return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+      return post.likes.find((like) => like === user?.result?._id)
         ? (
-          <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
+          <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `Vous et ${post.likes.length - 1} autres` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`}</>
         ) : (
           <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
         );
@@ -35,7 +35,7 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
-      {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+      {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator || user?.result?.role === 'admin') && (
         <div className={classes.overlay2}>
           <Button onClick={() => setCurrentId(post._id)} style={{ color: 'white' }} size="small">
             <MoreHorizIcon fontSize="default" />
@@ -53,7 +53,7 @@ const Post = ({ post, setCurrentId }) => {
         <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
           <Likes />
         </Button>
-        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator || user?.result?.role === 'admin') && (
           <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize="small" /> Supprimer
           </Button>
